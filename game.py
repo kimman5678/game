9f2a44857cc97e6857f07eeeefbe2c1db618eb57
20180366김우주 <kimman5678@gmail.com>
2021-04-28 20:39:55 +0900

    시험과제

--- game.py
+++ game.py
@@ -1,19 +1,30 @@
-  
-print("밥먹으러 갈까")
-print("무엇을 먹을 까")
-print("1.한우 2.삼겹 3.닭")
-a = input("입력: ")
-print("당신이 입력한 값은",a)
-#만약에 1번이면 한우
-if a == "1":
-    print("한우")
-#만약에 2번이면 삼겹
-if a == "2":
-    print("삼겹")
-#만약에 3번이면 닭
-if a == "3":
-    print("닭")
+input_id = input("아이디를 입력해주세요: ")
 
-#밥 정하기
-#메뉴 1,2,3 랜덤 선택
-#먹는 메뉴를 선택 해서 메뉴를 출력
+members = {'rladnwn':'김우주', 'dkdRh':'앙꼬', 'ad':'도지'} # id : 닉네임
+
+def login(input_id, members):
+    if input_id in members.keys():
+        print('Hello,', members[input_id])
+    else:
+        print('welcom')
+        nickname = input('닉네임 입력: ')
+        members[input_id] = nickname
+    return members[input_id], members
+
+nickname, members = login(input_id, members)
+
+print("{}게임을 시작합니다".format(nickname, nickname))
+print("길을 가다가 퉁퉁이를 만났습니다.")
+
+while True:
+    print("1. 싸운다 2. 도망간다")
+    num = int(input("선택: "))
+
+    if num == 1:
+        print("퉁퉁이에게 이긴다")
+        break
+    elif num == 2:
+        print("퉁퉁이에게 진다")
+        break
+    else:
+        print("잘못 입력하셨습니다. 다시 입력해주십시오.")
